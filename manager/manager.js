8baef91f898d1c2fad57d3ef7af077f4eb94c439
rubaalbrezat <rubaalbrezat@gmail.com>
2022-11-12 19:35:47 +0200

    edit

--- manager/manager.js
+++ manager/manager.js
@@ -1,27 +1,30 @@
+// const event=require('../events')
 const uuid = require('uuid');
 const uniqId = uuid.v4();
 const { faker } = require('@faker-js/faker')
-// const event = require('../event')
-const io = require('socket.io-client');
-const socket = io.connect(process.env.PORT)
+
+const socket = require('socket.io-client')
+const io = socket.connect('http://localhost:3000')
 
 
 
-setInterval(() => {
 
-	let info = {
-		ariline: 'Royal Jordanian Airlines',
-		id: uniqId,
-		pailotName: faker.name.fullName(),
-		distination: faker.address.county(),
-		time: faker.date.future()
 
-	}
-	console.log(`Manager: new flight  ${info.id}  have been scheduled Flight`)
-	socket.emit('new-flight', info)
 
-}, 10000);
 
-socket.on('arrived', (info) => {
-	console.log(`hi${info.pailotName},you are great thank you `)
+	setInterval(() => {
+		let detailes = {
+			airLine: 'Royal Jordanian Airlines',
+			idFligth: uniqId,
+			PailotName: faker.name.fullName(),
+			distination: faker.address.county(),
+			time: faker.date.future()
+		}
+		console.log(`Manager:new flight ${detailes.idFligth} have been schedule`)
+		io.emit('new-flight', detailes)
+
+	}, 10000);
+
+	io.on('arrived', (detailes) => {
+	console.log(`hello ${detailes.PailotName}, you are great thank you for youre effort`)
 })
